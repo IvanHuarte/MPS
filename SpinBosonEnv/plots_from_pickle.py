06f9345b90abcd31dc701268cb86bcdb7b274223
IvanHuarte <ivanhuarte@hotmail.com>
2026-07-05 18:46:56 +0200

    Update

--- SpinBosonEnv/plots_from_pickle.py
+++ SpinBosonEnv/plots_from_pickle.py
@@ -30,66 +30,68 @@ def plot_from_pickle(pickle_path, plot_field=True):
 
     nprime = wc / w0
 
-    fig1, ax1 = plt.subplots(figsize=[12, 8])
-    ax1.set_title(
-        r"$Occupation\;decayment \quad (n^\star=%.2f)$" % (nprime), fontsize=18
-    )
-    ax1.set_xlabel(r"$n$", fontsize=18)
-    ax1.set_ylabel(r"$\langle a_n^\dagger a_n \rangle_{GS}$", fontsize=18)
-    ax1.set_ylim(1e-18, 1e1)
-    ax1.set_xlim(9e-1, Nk // 2 + 2)
+    if plot_field:
 
-    ax1.vlines(nprime, 1e-18, 1e1, color="grey", ls="--")
-    ax1.text(
-        nprime + 1,
-        0.1,
-        r"$n^\star = \frac{\omega_c}{\omega_0}$",
-        transform=transforms.blended_transform_factory(ax1.transData, ax1.transAxes),
-        fontsize=12,
-    )
-    cmap = plt.colormaps["viridis"](np.linspace(0, 1, len(gvals)))
-
-    for i, g in enumerate(gvals):
-        Nx = np.loadtxt(
-            folder + "/GroundState_wc_%.4f_g_%.4f_NoccX.txt" % (wc, g),
-            dtype=complex,
-        ).real
-        Nxhalf = Nx[Nk // 2 :]
-        n_values = np.arange(len(Nxhalf))
-
-        # TEORIC
-        # n < nprime
-        Nteo_1 = g**2 / (np.pi**2 * w0**2 * nprime**2) * (1 / n_values**4)
-        # n > nprime
-        Nteo_2 = (
-            g**2
-            / (2 * np.pi * w0**2 * nprime**2)
-            * (np.exp(-2 * n_values / nprime) / n_values**3)
+        fig1, ax1 = plt.subplots(figsize=[12, 8])
+        ax1.set_title(
+            r"$Occupation\;decayment \quad (n^\star=%.2f)$" % (nprime), fontsize=18
         )
+        ax1.set_xlabel(r"$n$", fontsize=18)
+        ax1.set_ylabel(r"$\langle a_n^\dagger a_n \rangle_{GS}$", fontsize=18)
+        ax1.set_ylim(1e-18, 1e1)
+        ax1.set_xlim(9e-1, Nk // 2 + 2)
 
-        Nteo = np.concatenate([Nteo_1[1 : int(nprime)], Nteo_2[int(nprime) :]])
-        Nteo = (
-            g**2
-            / (np.pi**2 * w0**2 * nprime**2)
-            * (scipy.special.k1(n_values / nprime) / n_values) ** 2
+        ax1.vlines(nprime, 1e-18, 1e1, color="grey", ls="--")
+        ax1.text(
+            nprime + 1,
+            0.1,
+            r"$n^\star = \frac{\omega_c}{\omega_0}$",
+            transform=transforms.blended_transform_factory(ax1.transData, ax1.transAxes),
+            fontsize=12,
         )
-        ax1.plot(range(0, len(n_values)), Nteo, color=cmap[i])
+        cmap = plt.colormaps["viridis"](np.linspace(0, 1, len(gvals)))
 
-        # MPS RESULTS
-        ax1.plot(
-            range(len(Nxhalf)),
-            Nxhalf,
-            color=cmap[i],
-            marker="o",
-            ls="",
-            ms=3,
-            label=r"$g=%.2f$" % g,
-        )
+        for i, g in enumerate(gvals):
+            Nx = np.loadtxt(
+                folder + "/GroundState_wc_%.4f_g_%.4f_NoccX.txt" % (wc, g),
+                dtype=complex,
+            ).real
+            Nxhalf = Nx[Nk // 2 :]
+            n_values = np.arange(len(Nxhalf))
 
-    ax1.legend()
-    ax1.set_yscale("log")
-    ax1.set_xscale("log")
-    ax1.grid()
+            # TEORIC
+            # n < nprime
+            Nteo_1 = g**2 / (np.pi**2 * w0**2 * nprime**2) * (1 / n_values**4)
+            # n > nprime
+            Nteo_2 = (
+                g**2
+                / (2 * np.pi * w0**2 * nprime**2)
+                * (np.exp(-2 * n_values / nprime) / n_values**3)
+            )
+
+            Nteo = np.concatenate([Nteo_1[1 : int(nprime)], Nteo_2[int(nprime) :]])
+            Nteo = (
+                g**2
+                / (np.pi**2 * w0**2 * nprime**2)
+                * (scipy.special.k1(n_values / nprime) / n_values) ** 2
+            )
+            ax1.plot(range(0, len(n_values)), Nteo, color=cmap[i])
+
+            # MPS RESULTS
+            ax1.plot(
+                range(len(Nxhalf)),
+                Nxhalf,
+                color=cmap[i],
+                marker="o",
+                ls="",
+                ms=3,
+                label=r"$g=%.2f$" % g,
+            )
+
+        ax1.legend()
+        ax1.set_yscale("log")
+        ax1.set_xscale("log")
+        ax1.grid()
 
     # ENERGY
     gvals = data[:, 3]
